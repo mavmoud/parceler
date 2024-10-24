ae29bb77a9d6aaeb686021d1062d80e51a7724dd
Mahmoud Mohamed <mahmoud@null.net>
2024-10-24 01:44:49 -0400

    Updated UI with new gradient, header styling, and icons
    - Updated background color gradient
    - Styled header to match design mockups
    - Changed Favicon and website title
    - Updated homepage card icons
    - Added Inter and Montserrat fonts in app.css

--- DeliverySystem/src/Components/Header/Header.tsx
+++ DeliverySystem/src/Components/Header/Header.tsx
@@ -1,26 +1,62 @@
-import {AppBar, Box, Toolbar, Typography, Button, IconButton} from '@mui/material';
-import HomeIcon from '@mui/icons-material/Home';
-import { useNavigate } from 'react-router-dom';
-import { HOME_PAGE} from './constants';
-import { HOME_PAGE_URL } from '../../constants';
+import {AppBar, Box, Toolbar, Typography, Button} from '@mui/material';
+import {useNavigate} from 'react-router-dom';
+import {HOME_PAGE} from './constants';
+import {HOME_PAGE_URL} from '../../constants';
 import {HeaderButtonGroup} from "./HeaderButtonGroup.tsx";
 
-export const Header = ({ setSignInAuth, signInAuth }: { setSignInAuth: Function, signInAuth : boolean }) => {
+
+export const Header = ({setSignInAuth, signInAuth}: { setSignInAuth: Function, signInAuth: boolean }) => {
     const navigate = useNavigate();
     return (
-        <AppBar position="static" sx={{ background : 'none' , p: 3}}>
-            <Toolbar>
-                <IconButton color='inherit'
-                    onClick={() => navigate(HOME_PAGE_URL)}>
-                    <HomeIcon />
-                </ IconButton>
-                <Typography variant="h6" >{HOME_PAGE}</Typography>
-                <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center', pl: 13 }}>
-                    <Button color='inherit'>Track</Button>
-                    <Button color='inherit'>Ship</Button>
-                    <Button color='inherit'>Help</Button>
+        <AppBar position="static" sx={{background: 'none', width: '85%', mt: '45px', mx: 'auto', boxShadow: 'none'}}>
+            <Toolbar sx={{p: '0', display: 'flex', justifyContent: 'space-between', position: 'relative'}}>
+                <Box sx={{
+                    cursor: 'pointer',
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    gap: '14px',
+                    alignItems: 'center'
+                }} onClick={() => navigate(HOME_PAGE_URL)}>
+                    <img
+                        src={"Parceler.svg"}
+                        alt="Home Icon"
+                        style={{width: '40px', height: '40px', marginRight: '8px'}}
+                    />
+                    <Typography sx={{
+                        fontFamily: '"Montserrat", serif',
+                        fontSize: '18px',
+                        fontWeight: 'bold',
+                        letterSpacing: '20%'
+                    }}>{HOME_PAGE}</Typography>
                 </Box>
-                <HeaderButtonGroup setSignInAuth={setSignInAuth} signInAuth={signInAuth} />
+                <Box sx={{
+                    position: 'absolute',
+                    left: '50%',
+                    transform: 'translateX(-50%)',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    gap: '60px',
+                }}>
+                    <Button disableRipple color='inherit' sx={{
+                        fontFamily: '"Montserrat", serif',
+                        fontSize: '16px',
+                        fontWeight: 'medium',
+                        textTransform: 'none'
+                    }}>Track</Button>
+                    <Button disableRipple color='inherit' sx={{
+                        fontFamily: '"Montserrat", serif',
+                        fontSize: '16px',
+                        fontWeight: 'medium',
+                        textTransform: 'none'
+                    }}>Ship</Button>
+                    <Button disableRipple color='inherit' sx={{
+                        fontFamily: '"Montserrat", serif',
+                        fontSize: '16px',
+                        fontWeight: 'medium',
+                        textTransform: 'none'
+                    }}>Help</Button>
+                </Box>
+                <HeaderButtonGroup setSignInAuth={setSignInAuth} signInAuth={signInAuth}/>
             </Toolbar>
         </AppBar>
     );
