49067b10cee8d910d99cb0e963f90bf76f25946a
Gianluca Girardi <gl.girardi1@gmail.com>
2024-11-11 18:41:41 -0500

    Added dashboard re-routes

--- DeliverySystem/src/Hooks/constants.ts
+++ DeliverySystem/src/Hooks/constants.ts
@@ -2,3 +2,4 @@ export const USER_INFO: string = "userInfo";
 export const USER_TYPE_ID: string = "userTypeId";
 export const USER_FIRST_NAME: string = "firstName";
 export const USER_LAST_NAME: string = "lastName";
+export const USER_ID: string = "id";
