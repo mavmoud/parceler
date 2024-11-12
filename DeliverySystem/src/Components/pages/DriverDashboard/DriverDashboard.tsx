9283fcaeff1cdda1a2722b66fae0e2d59b93d290
Gianluca Girardi <gl.girardi1@gmail.com>
2024-11-11 23:58:03 -0500

    Created driver dashboard order list

--- DeliverySystem/src/Components/pages/DriverDashboard/DriverDashboard.tsx
+++ DeliverySystem/src/Components/pages/DriverDashboard/DriverDashboard.tsx
@@ -4,11 +4,13 @@ import {
   useAuthentication,
   ROLE_DRIVER,
 } from "../../../Hooks/useAuthentication";
+import { BACKGROUND_RIGHT, IMAGE3 } from "../../../constants";
+import { DriverDashboardOrderList } from "./DriverDashboardOrderList";
 import {
-  BACKGROUND_RIGHT,
-  BACKGROUND_BOTTOM,
-  IMAGE3,
-} from "../../../constants";
+  USER_FIRST_NAME,
+  USER_LAST_NAME,
+  USER_INFO,
+} from "../../../Hooks/constants";
 
 export const DriverDashboard = () => {
   useBodyBackground({
@@ -17,13 +19,22 @@ export const DriverDashboard = () => {
     backgroundSize: "cover",
   });
 
-  const userInfo = useAuthentication(ROLE_DRIVER);
+  const driver = useAuthentication(ROLE_DRIVER);
 
   return (
-    <Grid2>
-      <Typography variant="h1" color="white">
-        DRIVER DASHBOARD
+    <Grid2
+      container
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Typography variant="h2" color="white" align="center">
+        {`${driver?.[USER_INFO]?.[USER_FIRST_NAME]} ${driver?.[USER_INFO]?.[USER_LAST_NAME]}`}{" "}
+        <br /> Assigned Deliveries
       </Typography>
+      <Grid2 container justifyContent="center" sx={{ width: "55rem", mt: 4 }}>
+        <DriverDashboardOrderList />
+      </Grid2>
     </Grid2>
   );
 };
