e0f85feafad4c0729fbc12b5ec882a5a89ba1cc4
Mahmoud Mohamed <mahmoud@null.net>
2024-11-27 23:06:40 -0500

    Updated styling in admin dashboard

--- DeliverySystem/src/Components/pages/AdminDashboard/AdminDashboardAccounts.tsx
+++ DeliverySystem/src/Components/pages/AdminDashboard/AdminDashboardAccounts.tsx
@@ -1,4 +1,4 @@
-import { Grid2, Typography } from "@mui/material";
+import { Box, Grid2, Typography } from "@mui/material";
 import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
@@ -16,40 +16,88 @@ export const AdminDashboardAccounts = () => {
   const user = useAuthentication(ROLE_ADMIN);
   const { data: users, isLoading } = useQuery(
     ["users", user?.userInfo?.id],
-    () => UserService.GetAllUsers()
+    () => UserService.GetAllUsers(),
   );
 
   return (
-    <Grid2 className="container-content" sx={{ width: "100%" }}>
-      {!isLoading &&
-        users &&
-        users?.map((user, index) => {
-          return (
-            <Accordion
-              key={user.email}
-              sx={{
-                bgcolor: "#FFFFFF",
-                color: "black",
-                border: "1px solid rgba(0, 0, 0, .125)",
-                boxShadow: 2,
-              }}
-            >
-              <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                {`Account ${++index}: ${user.firstName} ${user.lastName}`}
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography p={1} variant="h6">{`Account Details`}</Typography>
-                <Typography pl={1}>{`Account type: ${userTypeMapper.get(
-                  user.userTypeId
-                )}`}</Typography>
-                <Typography pl={1}>{`Email: ${user.email}`}</Typography>
-                <Typography
-                  pl={1}
-                >{`Phone number: ${user.phoneNumber}`}</Typography>
-              </AccordionDetails>
-            </Accordion>
-          );
-        })}
+    <Grid2
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        width: "fit-content",
+        mb: "30px",
+      }}
+    >
+      <Box
+        sx={{
+          backgroundColor: "white",
+          p: "25px",
+          borderRadius: "50px",
+          height: "400px",
+          overflowX: "auto",
+        }}
+      >
+        {!isLoading &&
+          users &&
+          users?.map((user, index) => {
+            return (
+              <Accordion
+                key={user.email}
+                sx={{
+                  boxShadow: "none",
+                  color: "black",
+                  border: "1px solid rgba(0, 0, 0, .125)",
+                  borderRadius: "35px",
+                  width: "800px",
+                  mb: "10px",
+                  "&:first-of-type": {
+                    borderTopLeftRadius: "35px",
+                    borderTopRightRadius: "35px",
+                  },
+                  "&:last-of-type": {
+                    borderBottomLeftRadius: "35px",
+                    borderBottomRightRadius: "35px",
+                  },
+                  "&:before": {
+                    display: "none",
+                  },
+                }}
+              >
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  sx={{
+                    height: "50px",
+                    pl: "20px",
+                    fontFamily: '"Montserrat", serif',
+                  }}
+                >
+                  {`Account ${++index}: ${user.firstName} ${user.lastName}`}
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography
+                    p={1}
+                    variant="h6"
+                    sx={{ fontFamily: '"Montserrat", serif' }}
+                  >{`Account Details`}</Typography>
+                  <Typography
+                    pl={1}
+                    sx={{ fontFamily: '"Montserrat", serif' }}
+                  >{`Account type: ${userTypeMapper.get(
+                    user.userTypeId,
+                  )}`}</Typography>
+                  <Typography
+                    pl={1}
+                    sx={{ fontFamily: '"Montserrat", serif' }}
+                  >{`Email: ${user.email}`}</Typography>
+                  <Typography
+                    pl={1}
+                    sx={{ fontFamily: '"Montserrat", serif' }}
+                  >{`Phone number: ${user.phoneNumber}`}</Typography>
+                </AccordionDetails>
+              </Accordion>
+            );
+          })}
+      </Box>
     </Grid2>
   );
 };
--- DeliverySystem/src/Components/pages/AdminDashboard/AdminDashboardOrders.tsx
+++ DeliverySystem/src/Components/pages/AdminDashboard/AdminDashboardOrders.tsx
@@ -1,4 +1,4 @@
-import { Grid2, Typography } from "@mui/material";
+import { Box, Grid2, Typography } from "@mui/material";
 import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
@@ -17,45 +17,92 @@ export const AdminDashboardOrders = () => {
   const user = useAuthentication(ROLE_ADMIN);
   const { data: orders, isLoading } = useQuery(
     ["orders", user?.userInfo?.id],
-    () => OrderService.GetAllOrders()
+    () => OrderService.GetAllOrders(),
   );
 
   return (
-    <Grid2 className="container-content">
-      {!isLoading &&
-        orders &&
-        orders?.map((order: object, index: number) => {
-          const trackingNumber = order?.[ORDER_TRACKING_NUMBER];
-          return (
-            <Accordion key={trackingNumber} className="container-accordion">
-              <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                <Typography>{`Order ${++index}: ${trackingNumber}`}</Typography>
-              </AccordionSummary>
-              <AccordionDetails className="container-accordion-details">
-                <Grid2 container>
-                  <AdminCustomCard
-                    title={"Recipient"}
-                    first={`First name: ${order.recipientFirstName}`}
-                    second={`Last name ${order.recipientLastName}`}
-                    third={`Address: ${order.recipientAddress}`}
-                  />
-                  <AdminCustomCard
-                    title={"Sender"}
-                    first={`First name: ${order.senderFirstName}`}
-                    second={`Last name ${order.senderLastName}`}
-                    third={`Contact: ${order.senderPhoneNumber}`}
-                  />
-                  <AdminCustomCard
-                    title={"Order details"}
-                    first={`Weight: ${order.packageWeight} kg`}
-                    second={`Dimensions: ${order.packageDimension}`}
-                    third={`Cost: ${order.amount} ${order.currency}`}
-                  />
-                </Grid2>
-              </AccordionDetails>
-            </Accordion>
-          );
-        })}
+    <Grid2
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        width: "fit-content",
+        mb: "30px",
+      }}
+    >
+      <Box
+        sx={{
+          backgroundColor: "white",
+          p: "25px",
+          borderRadius: "50px",
+          overflowX: "auto",
+          height: "400px",
+        }}
+      >
+        {!isLoading &&
+          orders &&
+          orders?.map((order: object, index: number) => {
+            const trackingNumber = order?.[ORDER_TRACKING_NUMBER];
+            return (
+              <Accordion
+                key={trackingNumber}
+                sx={{
+                  backgroundColor: "white",
+                  boxShadow: "none",
+                  color: "black",
+                  border: "1px solid rgba(0, 0, 0, .125)",
+                  borderRadius: "35px",
+                  width: "800px",
+                  mb: "10px",
+                  "&:first-of-type": {
+                    borderTopLeftRadius: "35px",
+                    borderTopRightRadius: "35px",
+                  },
+                  "&:last-of-type": {
+                    borderBottomLeftRadius: "35px",
+                    borderBottomRightRadius: "35px",
+                  },
+                  "&:before": {
+                    display: "none",
+                  },
+                }}
+              >
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  sx={{
+                    height: "50px",
+                    pl: "20px",
+                  }}
+                >
+                  <Typography
+                    sx={{ fontFamily: '"Montserrat", serif' }}
+                  >{`Order ${++index}: ${trackingNumber}`}</Typography>
+                </AccordionSummary>
+                <AccordionDetails className="container-accordion-details">
+                  <Grid2 container justifyContent={"space-around"}>
+                    <AdminCustomCard
+                      title={"Recipient"}
+                      first={`First name: ${order.recipientFirstName}`}
+                      second={`Last name: ${order.recipientLastName}`}
+                      third={`Address: ${order.recipientAddress}`}
+                    />
+                    <AdminCustomCard
+                      title={"Sender"}
+                      first={`First name: ${order.senderFirstName}`}
+                      second={`Last name: ${order.senderLastName}`}
+                      third={`Contact: ${order.senderPhoneNumber}`}
+                    />
+                    <AdminCustomCard
+                      title={"Order details"}
+                      first={`Weight: ${order.packageWeight} kg`}
+                      second={`Dimensions: ${order.packageDimension}`}
+                      third={`Cost: ${order.amount} ${order.currency}`}
+                    />
+                  </Grid2>
+                </AccordionDetails>
+              </Accordion>
+            );
+          })}
+      </Box>
     </Grid2>
   );
 };
