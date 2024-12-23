c6d1b647719af1ebf11dae614f3ae305591f7797
Mahmoud Mohamed <mahmoud@null.net>
2024-12-23 03:14:52 -0500

    Backend optimization for deployment

--- DeliverySystemAPI/server.ts
+++ DeliverySystemAPI/server.ts
@@ -32,5 +32,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on port:${port}`);
 });
